127a5f6969cbd2be6474762c7e00ec7f8efed693
Clement_Guichard <c.guichard508@laposte.net>
2016-12-09 21:33:51 +0100

    Initialisation des headers

---
+++ termSaver1.h
@@ -0,0 +1,12 @@
+#ifndef TERMSAVER1_H_INCLUDED
+#define TERMSAVER1_H_INCLUDED
+
+void rechercheRepertoire();
+void selectionAleatoire();
+void chargementBitmap();
+void affichageBitmap();
+void toucheDeFin();
+void Reset();
+
+
+#endif // TERMSAVER1_H_INCLUDED
