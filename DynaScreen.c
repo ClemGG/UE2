08ac8bd5a3bbba1e69ff89d807c67c2d62b6a6bb
exia <exia@WSSYSCP1.exia>
2016-12-16 09:35:44 +0100

    Terminé! Enfin! (2.0)

--- DynaScreen.c
+++ DynaScreen.c
@@ -8,8 +8,8 @@
 
 char timeString[7];
 
-void loadingBar();
-void tUpdate();
+void loadingBar();//fonction d'actualisation du message d'attente
+void tUpdate();//fonction d'actualisation de l'heure
 
 int main(){
 	char c;
@@ -20,11 +20,11 @@ int main(){
 	xySlide(0,-9);
 
 	while(c == 0){
-
+//On écrit chaque caractère un à un, de sorte à affichier l'heure instantanée
 		printFile('c',0,33);
 		printFile('c',0,44);
 
-		tUpdate();
+		tUpdate();//On actualise l'heure
 		for(i=0; i<6; i++){
 			if(i == 2 || i == 4){j++;}
 			printFile('n',timeString[i]-'0',j*4);
@@ -33,32 +33,36 @@ int main(){
 		j = CENTER;
 		r = CENTER;
 		P = CENTER;
-		Historique(2,r,P);
+		Historique(2,r,P);//On enregistre le format de centrage dans le fichier texte
 
-		loadingBar();
+		loadingBar();//On lance la fonction concernant le message
 	}
 	return 0;
 }
 
 void tUpdate(){
+//On définit les variables liées au temps
 	time_t current_time;
 	struct tm * time_info;
-
+//On récupère l'heure système
 	time(&current_time);
 	time_info = localtime(&current_time);
-
+//On l'affiche à l'écran
 	strftime(timeString, sizeof(timeString), "%H%M%S", time_info);
 }
 
 void loadingBar(){
 	int i;
+//On place le message d'actualisation en bas de l'écran
 	xySlide(-80,-30);
 	printf("Actualisation ");
+//A chaque seconde, on rajoute un point, jusqu'à ce que i = constante d'actualisation définie plus haut
 	for(i=0; i<ACTUALISATION; i++){
 		printFile('c',1,i);
 		xySlide(0,-4);
 		sleep(1);
 	}
+// Lorsque i = ACTUALISATION, on vide l'écran
 	xySlide(-80,13);
 	printf("\033[2J");
 }
--- InteraScreen.c
+++ InteraScreen.c
@@ -7,43 +7,63 @@
 int main(){
 
 	system("clear");
-
+///On définit une variable qui servira à détecter les entrées clavier
 	char c;
 
+//On définit la position aléatoire de l'avion
 	int AvionX=rng(70);
 	int AvionY=-rng(25);
+
+//On l'enregistre dans Historique.txt
 	Historique(3,AvionX,AvionY);
+
+//On affiche l'avion à l'écran
 	xySlide(0,AvionY);
 	printFile('a',rng(3),AvionX);
 
+//Tant que l'on a pas entré la touche q (quitter)
 	while(c != 'q'){
 		//xySlide(-80,-30);
 		c = getchar();
+
+//Si on appuie sur la touche de droite
 		if(c == 'h'){
 			system("clear");
+//On change la position de l'avion
 			AvionX++;
+//On décale l'avion d'une unité vers la droite
 			xySlide(AvionX,AvionY);
 			printFile('a',1,AvionX);
 		}
+//Touche de gauche
 		else if (c == 'f'){
 			system("clear");
+//Changement de position
 			AvionX--;
+//Déplacement vers la gauche
 			xySlide(AvionX,AvionY);
 			printFile('a',3,AvionX);
 		}
+//Touche de bas
 		else if (c == 'g'){
 			system("clear");
+//Changement de position
 			AvionY--;
+//Déplacement vers le bas
 			xySlide(AvionX,AvionY);
 			printFile('a',2,AvionX);
 		}
+//Touche du haut
 		else if (c == 't'){
 			system("clear");
+//Changement de position
 			AvionY++;
+//Déplacement vers le haut
 			xySlide(AvionX,AvionY);
 			printFile('a',0,AvionX);
 		}
 	}
 	xySlide(-80,0);
+//On vide l'écran lorsqu'on entre la touche q
 	system("clear");
 }
